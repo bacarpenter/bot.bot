91ab09f2a6c34c388117233fb91471298c5962a4
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-13 14:04:35 -0400

    added parse function with enum

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
+from typing import Dict
+from todoList import addTodo
 import discord
 import os
+from enum import Enum
 
-RESPOND_TO_UNKNOWN_MESSAGE = False
 
-client = discord.Client()
+class MessageType(Enum):
+    HELLO = 0
+    BYE = 1
+    NEW_TASK = 2
+
 
 message_types = {
     "hello": ["hi", "hello", "howdy", "hey", "greetings"],
     "bye": ["bye", "latter", "goodnight", "ttyl", "see you soon"],
     "new_task": ["Add a to do", "new todo", "todo", "remind me", "to do"],
 }
+RESPOND_TO_UNKNOWN_MESSAGE = True
+
+client = discord.Client()
 
 
 @client.event
@@ -19,22 +28,63 @@ async def on_ready():
 
 # Handle types of messages
 @client.event
-async def on_message(message):
+async def on_message(message) -> None:
+    parsed = parseMessage(message.content)
     if message.author == client.user:  # Don't respond to own message
         return
-
-    if message.content.lower() in message_types['hello']:
+    if parsed == None and RESPOND_TO_UNKNOWN_MESSAGE:
+        await message.channel.send("Sorry, I don't recognize \"{0}\"".format(message.content))
+    elif parsed == None and not RESPOND_TO_UNKNOWN_MESSAGE:
+        return
+    elif parsed['command'] == MessageType.HELLO:
         await message.channel.send(f"Hey {message.author.name}!")
 
-    elif message.content.lower() in message_types['bye']:
+    elif parsed['command'] == MessageType.BYE:
         await message.channel.send(f"I'll talk to you later, {message.author.name}!")
 
-    elif message.content.lower() in message_types['new_task']:
+    elif parsed['command'] == MessageType.NEW_TASK:
+        addTodo(parsed['value'])
         await message.channel.send(f"Copy that! I'll add this to your to do list, {message.author.name}!")
 
-    elif RESPOND_TO_UNKNOWN_MESSAGE:  # Togle weather or not to respond to unknown messages
+    elif RESPOND_TO_UNKNOWN_MESSAGE:  # Toggle weather or not to respond to unknown messages
         # Just using format here because I want to
         await message.channel.send("Sorry, I don't recognize \"{0}\"".format(message.content))
+
+"""
+Return message as a dict with a command and the value
+"""
+
+
+def parseMessage(message) -> Dict:
+    parsed = {}
+    cmd = None
+    try:
+        cmd = message[0:message.index(":")]
+    except ValueError:
+        cmd = message
+        if cmd.lower() in message_types['hello']:
+            parsed['command'] = MessageType.HELLO
+            parsed['value'] = None
+            return parsed
+        elif cmd.lower() in message_types['bye']:
+            parsed['command'] = MessageType.BYE
+            parsed['value'] = None
+            return parsed
+        else:
+            return None
+
+    if cmd.lower() in message_types['hello']:
+        parsed['command'] = MessageType.HELLO
+    elif cmd.lower() in message_types['bye']:
+        parsed['command'] = MessageType.BYE
+    elif cmd.lower() in message_types['new_task']:
+        parsed['command'] = MessageType.NEW_TASK
+    else:
+        return None
+
+    parsed['value'] = message[message.index(":") + 2:]  # +2 for the space
+    return parsed
+
 
 # Get OAuth Token
 try:
---
+++ todoList.py
@@ -0,0 +1,2 @@
+def addTodo(task):
+    print(f"New todo: {task}")
