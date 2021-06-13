d0987d56b341d52a0b080c85010986f74b1d91de
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-13 15:34:51 -0400

    Added todo db functionality

--- todoList.py
+++ todoList.py
@@ -1,9 +1,27 @@
+from typing import List
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# Init access to db
+cred = credentials.Certificate("secrets/bot-bot-firebase-adminsdk.json")
 firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
-def addTodo(task):
-    print(f"New todo: {task}")
+
+def addTodo(task) -> None:
+    # Get next todo id
+    nextId = int(db.collection('counter').document(
+        'counter').get().to_dict()['counter']) + 1
+
+    # Create a new todo entry
+    todo_ref = db.collection('tasks').document(f"task#{nextId}")
+    todo_ref.set({'task': task, 'done': False})
+
+    # +1 to counter
+    db.collection('counter').document('counter').set({'counter': nextId})
+
+
+def readAll() -> List:
+    pass
