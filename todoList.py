a990f85917487c80392112cb5e98cb5e9a3ae87d
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-13 14:18:54 -0400

    Prep for Firestore

--- todoList.py
+++ todoList.py
@@ -1,2 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+
 def addTodo(task):
     print(f"New todo: {task}")
