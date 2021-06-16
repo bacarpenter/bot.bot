966374f9143cadf8aeda8a3130edee8a3b55b545
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-15 22:33:10 -0400

    CLI improvement
    Fixed keyboard interupt error.

--- cli.py
+++ cli.py
@@ -3,10 +3,16 @@ import bot
 
 def main():
     while(True):
-        message = input("Message => ")
-        responses = bot.respond(message)
-        for response in responses:
-            print(response)
+        try:
+            message = input("Message => ")
+            responses = bot.respond(message)
+            for response in responses:
+                print(response)
+        except KeyboardInterrupt:
+            responses = bot.respond("bye")
+            for response in responses:
+                print("\n", response)
+                exit(0)
 
 
 if __name__ == "__main__":
