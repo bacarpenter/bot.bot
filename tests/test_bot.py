6295cc90500190f3a715c7eab8708df132c45ad9
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-17 00:17:22 -0400

    um, unittest runs test alpha. why>

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -4,7 +4,7 @@ import bot
 
 
 class TestPleasantries(unittest.TestCase):
-    def test_hello(self):
+    def test_0hello(self):
         # Force settings for testing constancy
         bot.settings = {
             "user_name": "user",
@@ -13,7 +13,7 @@ class TestPleasantries(unittest.TestCase):
         result = bot.respond("hello")
         self.assertEqual(result, ['Hello, user!'])
 
-    def test_goodbye(self):
+    def test_1goodbye(self):
         bot.settings = {
             "user_name": "user",
             "reply_to_unknown": True
@@ -21,7 +21,7 @@ class TestPleasantries(unittest.TestCase):
         result = bot.respond("bye")
         self.assertEqual(result, ["I'll talk to you later, user"])
 
-    def test_thanks(self):
+    def test_2thanks(self):
         bot.settings = {
             "user_name": "user",
             "reply_to_unknown": True
@@ -29,7 +29,7 @@ class TestPleasantries(unittest.TestCase):
         result = bot.respond("thank you")
         self.assertEqual(result, ["my pleasure! 🥳"])
 
-    def test_reply_unknown(self):
+    def test_3reply_unknown(self):
         bot.settings = {
             "user_name": "user",
             "reply_to_unknown": True
@@ -37,7 +37,7 @@ class TestPleasantries(unittest.TestCase):
         result = bot.respond("1234")
         self.assertEqual(result, ["Sorry, I don't understand \"1234\""])
 
-    def test_no_reply_unknown(self):
+    def test_4no_reply_unknown(self):
         bot.settings = {
             "user_name": "user",
             "reply_to_unknown": False
@@ -49,7 +49,7 @@ class TestPleasantries(unittest.TestCase):
 class TestTodo(unittest.TestCase):
     task_id = -1
 
-    def test_add(self):
+    def test_0add(self):
         global task_id
         bot.settings = {
             "user_name": "user",
@@ -61,9 +61,8 @@ class TestTodo(unittest.TestCase):
         self.assertEqual(
             result[0], "Copy that! I'll add this to your to do list, user!")
         self.assertEqual(len(result), 2)
-        print(task_id)
 
-    def test_read(self):
+    def test_1read(self):
         bot.settings = {
             "user_name": "user",
             "reply_to_unknown": True
@@ -72,11 +71,22 @@ class TestTodo(unittest.TestCase):
         self.assertEqual(
             result[-1], f"Task #{task_id}: TEST_TODO\tStatus: todo")
 
-    # def test_complete(self):
-    #     bot.settings = {
-    #         "user_name": "user",
-    #         "reply_to_unknown": True
-    #     }
+    def test_2complete(self):
+        bot.settings = {
+            "user_name": "user",
+            "reply_to_unknown": True
+        }
+        result = bot.respond("read all")
+        self.assertEqual(
+            result[-1], f"Task #{task_id}: TEST_TODO\tStatus: todo")  # Make sure it's not already done
+
+        result = bot.respond(f"complete: {task_id}")
+        self.assertEqual(
+            result, ['Got it!', f'Task #{task_id}: TEST_TODO\tStatus: done'])
+
+        result = bot.respond("read all")
+        self.assertEqual(
+            result[-1], f"Task #{task_id}: TEST_TODO\tStatus: done")
 
     # def test_remove(self):
     #     bot.settings = {
