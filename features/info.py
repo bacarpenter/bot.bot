1b09b887071e2ed84cb3e01d1efb764bb47e4e00
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-17 21:58:33 -0400

    Formating changes

--- features/info.py
+++ features/info.py
@@ -5,6 +5,6 @@ from typing import List
 
 def info(messge, settings) -> List:
     hello = "Hello! I'm bot.bot, an extendable bot built by Ben Carpenter for use with Discord."
-    commands = "You can ask me to do things like add a todo: [TASK], complete: [TASK_ID] or just exchange some pleasantries."
+    commands = "You can ask me to do things like add a `todo: [TASK]`, `complete: [TASK_ID]` or just exchange some pleasantries."
     link = "Check me out on GitHub! https://github.com/bacarpenter/bot.bot"
     return [hello, commands, link]
