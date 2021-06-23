7e11f4fa12e44fa891b65a06d5b06ae9cf9498e2
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-23 01:03:02 -0400

    Loading secrets on CLI server

--- discord_interface.py
+++ discord_interface.py
@@ -31,16 +31,21 @@ async def on_raw_reaction_add(reaction):
     pass  # TODO and BUG
 
 
-# Load OAuth from JSON
-try:
-    with open("secrets/discord_token.json") as settings_JSON:
-        TOKEN = json.load(settings_JSON)["token"]
-except FileNotFoundError:
-    print("Error: Could not open secrets/discord_token.json Is it there?")
-    exit(1)
-except KeyError:
-    print("Error: secrets/discord_token.json does not have a token object.")
-    exit(2)
+# See if running on CI server
+if os.environ.get("CI") == "true":
+    # Should only be run if project is on a CI server. Namely, GitHub actions
+    TOKEN = os.environ.get("DISCORD_TOKEN")
+else:
+    # Load OAuth from JSON
+    try:
+        with open("secrets/discord_token.json") as settings_JSON:
+            TOKEN = json.load(settings_JSON)["token"]
+    except FileNotFoundError:
+        print("Error: Could not open secrets/discord_token.json Is it there?")
+        exit(1)
+    except KeyError:
+        print("Error: secrets/discord_token.json does not have a token object.")
+        exit(2)
 
 
 client.run(TOKEN)
