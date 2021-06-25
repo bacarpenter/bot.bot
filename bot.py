97e9bdb78e3bc43b8c30ab52973cb73d2d16cee3
Ben Carpenter <bacarpenter04@gmail.com>
2021-06-25 00:47:21 -0400

    First few instructions in!

--- bot.py
+++ bot.py
@@ -12,8 +12,6 @@ import json
 with open("settings.json") as settings_JSON:
     settings = json.load(settings_JSON)
 
-next_require_confirmation: bool = False
-
 
 def respond(message) -> List:
     """
@@ -22,6 +20,11 @@ def respond(message) -> List:
     as a "disbatch" station, for context specific methods. That way, I can avoid a very
     long elif statement, and seporate out functions.
     """
+
+    # Bot setup subsection:
+    if settings.get('setup'):
+        return bot_setup(message)
+
     message_type = assign_type(message)
 
     if message_type is None:
@@ -44,3 +47,48 @@ def respond(message) -> List:
     }
 
     return response_methods[message_type](message, settings)
+
+
+setup_stage = 0
+
+
+def bot_setup(message):
+    global setup_stage
+    rList = []
+    if setup_stage == 0:
+        rList.append(
+            "Hello! I'm bot.bot. I can't wait to help you out, but first we need to get some thing's set up.")
+        rList.append("First, what's your name?")
+        setup_stage += 1
+        return rList
+    elif setup_stage == 1:
+        # Place holder. Write name to the settings.json
+        rList.append(f"Awesome! Hello {message}")
+        rList.append("Next, would you like me to reply to messages I don't understand? Or just ignore them? I will confirm every message I do understand either way. (yes/no)")
+        setup_stage += 1
+        return rList
+    elif setup_stage == 2:
+        # Place holder. Write respond setting to the settings.json
+        rList.append(f"Got it. I will {'not' if message is 'yes' else ''}")
+        rList.append("Next, we will need to set up a database.")
+        rList.append(
+            "**Step 1**: Login / create a Firebase (https://firebase.google.com/) account")
+        rList.append("**Step 2**: Create a project with the \"add project\" button in the console (https://console.firebase.google.com/) This can be named what ever you want. I named mine \"bot-bot\"")
+        rList.append(
+            "**Step 3**: When prompted, turn off the \"Enable Google Analytics for this project\" toggle. Then chose the big, blue, create project button")
+        rList.append("Ready to keep going? (Yes/No)")
+        setup_stage += 1
+        return rList
+    elif setup_stage == 3:
+        if message is "no":
+            rList.append(
+                "Oh no. I'm sorry to hear somethings not right. Please create an issue at https://github.com/bacarpenter/bot.bot/issues/.")
+            setup_stage = 0
+            return rList
+
+        rList.append(
+            "Great! Let's keep moving. Next, you will add the firestore database to this project.")
+        rList.append("**Step 4**: Select \"Firestore Database\" from the left side menu, under the build subsection. Then, select the white \"Create database\" button")
+        rList.append(
+            "**Step 5**: Choose start in production and hit next. Then choose a location close to you. Select enable.")
+        # FIXME continue from here!
